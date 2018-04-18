c0fe29acb9d86cd7e3a54be5e6fb9c42ec4dfe89
Nyssa Keller <nyssa1@gmail.com>
2018-04-18 16:03:07 -0600

    Change to arrow functions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket){  
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });

720a2ac0b7f606969c4a83266e238e85a9467471
Nyssa Keller <nyssa1@gmail.com>
2018-04-18 15:52:30 -0600

    Function in html

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+
+  io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });

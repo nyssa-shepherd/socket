23ac5e5cee3dc652b21b62231049c06be9b68933
Nyssa Keller <nyssa1@gmail.com>
2018-04-18 16:26:54 -0600

    User typing && user disconnected

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){  
+io.on('connection', function(socket){
+  io.emit('connect message', 'Another user has connected.');
+
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
@@ -15,11 +17,20 @@ io.on('connection', function(socket){
     socket.on('chat message', function(msg){
       io.emit('chat message', msg);
     });
+
+    socket.on('typing', user => {
+      io.emit('typing', user);
+    });
+  
+    socket.on('done typing', user => {
+      io.emit('done typing', user);
+    });
   });
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('disconnect', () => {
+    io.emit('disconnect message', 'The user disconnected');
   });
+
 });
 
 http.listen(3000, function(){
